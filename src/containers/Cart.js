432c69d0175920f8601c3af2dd3e97d8fb39d7f3
venkey45R <venkateshamulraj@gmail.com>
2024-04-23 19:57:55 +0530

    Updated code to include new feature

--- src/containers/Cart.js
+++ src/containers/Cart.js
@@ -22,29 +22,29 @@ const Cart = () => {
   return (
     <div className="pb-10 bg-indigo-200 -mt-12 md:-mt-4 min-h-screen">
       <div className="mx-2 my-1 md:my-4 mt-12 pt-0 md:pt-10 border border-indigo-200">
-        <h1 className="text-xl font-bold text-blue-900 ml-20 pb-6">YOUR ITEMS:</h1>
-        <div className="flex justify-between mx-20">
-          <div className="block bg-white rounded-xl px-10 pb-6">
+        <h1 className="text-xl font-bold text-blue-900 ml-4 md:ml-20 pb-2 md:pb-6">YOUR ITEMS:</h1>
+        <div className="block md:flex justify-between mx-4 md:mx-20">
+          <div className="block bg-indigo-200 md:bg-white rounded-xl px-4 md:px-10 pb-6">
             {cartProducts.map((product, index) => (
-              <div key={index} className="flex justify-between mt-10 bg-white max-w-3xl pb-4 border border-blue-900 py-5 rounded-xl">
-                <img src={product.image} alt="cart" className="w-20 h-20 ml-8 mr-4 my-4 md:my-5" />
-                <div className="flex mt-2 md:mt-8 ml-1">
-                  <h1 className="text-sm max-w-52 mx-2">{product.title}</h1>
-                  <h2 className="text-sm mx-2">Price: $ {product.price}</h2>
-                  <div className=" py-1">
-                    <button className="rounded-lg p-3 -mt-2 font-bold ml-5 bg-blue-900 text-white">Proceed to checkout</button>
+              <div key={index} className="block md:flex justify-between mt-10 bg-white max-w-3xl pb-4 border border-blue-900 py-5 rounded-xl">
+                <img src={product.image} alt="cart" className="w-28 md:w-20 h-28 md:h-20 ml-auto md:ml-8 mr-auto md:mr-4 my-4 md:my-5" />
+                <div className="block md:flex mt-2 md:mt-8 ml-1">
+                  <h1 className="text-sm max-w-none md:max-w-52 my-4 md:my-0 mx-2 md:mx-2">{product.title}</h1>
+                  <h2 className="text-sm my-4 md:my-0 mx-2 md:mx-2">Price: $ {product.price}</h2>
+                  <div className=" py-1"> 
+                    <button className="rounded-lg px-14 py-4 md:px-3 md:py-3 mt-2 md:-mt-2 font-bold ml-4 mr-4 md:mr-0 md:ml-5 bg-blue-900 text-white">Proceed to checkout</button>
                   </div>
-                  <button className=" mx-8 text-2xl bg-white font-bold text-red-500 relative -top-6" onClick={() => handleRemoveCart(product)}><i class="fa-solid fa-xmark"></i></button>
+                  <button className=" mx-8 text-2xl bg-white font-bold text-red-500 relative md:-top-8 -top-72 md:right-0 -right-52" onClick={() => handleRemoveCart(product)}><i class="fa-solid fa-xmark"></i></button>
                 </div>
               </div>
             ))}
           </div>
-          <div className="block mt-2 bg-white pt-10 px-10 mr-3 rounded-xl h-4/5">
+          <div className="block mt-2 bg-white pt-10 px-4 md:px-10 mr-4 border border-blue-900 ml-2 md:ml-0 rounded-xl h-4/5">
             <div className="block py-4">
               <h2>PROMO CODE:</h2>
-              <input type="text" placeholder="Promo code" className="w-80 h-12 rounded-xl px-2 bg-indigo-200 mt-2" />
+              <input type="text" placeholder="Promo code" className=" w-64 md:w-80 h-12 rounded-xl px-2 bg-indigo-200 mt-2" />
             </div>
-            <h2 className="text-lg mt-4">Order Summary:</h2>
+            <h2 className="text-lg mt-2 md:mt-4">Order Summary:</h2>
             <div className=" flex justify-between mx-3 my-2">
               <h2 className="text-lg">Sub Total:</h2><h2>${total}</h2>
             </div>
