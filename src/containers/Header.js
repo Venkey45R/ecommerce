8c84450cef6a0841961e68f69766905f4a5b7168
venkey45R <venkateshamulraj@gmail.com>
2024-04-23 20:03:15 +0530

    Updated code to include new feature

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -49,7 +49,7 @@ const Header = () => {
               LOGIN
             </button>
           </Link>
-          <div className="md:hidden cursor-pointer relative items-center -top-0" onClick={toggleMobileMenu}>
+          <div className="md:hidden cursor-pointer relative items-center" onClick={toggleMobileMenu}>
             <i className="fa-solid fa-bars text-lg text-white"></i>
           </div>
         </div>
